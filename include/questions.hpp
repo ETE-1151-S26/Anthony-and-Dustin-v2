57ba7297a56d07fff424656214e3dc138a997b09
DEguiluz1995 <Dleguiluz@cpp.edu>
2026-02-21 00:40:21 -0800

    Blackjack almost done

--- include/questions.hpp
+++ include/questions.hpp
@@ -3,19 +3,20 @@
 
 //Display casino menu
 void showMainMenu();
+int getMenuChoice();//Reads and validates user input for menu choice
 
-//Reads and validates user input for menu choice
-int getMenuChoice();
+//Function for processing user input
+int readInt();
 
-//Starts blackjack game
-void startBlackjack();
+//Starts blackjack game w/ bankroll
+void startBlackjack(int &bankroll);
 
 //Functions for managing shoe (multiple decks) 
 void buildShoe(int shoe[], int shoeSize, int numDecks);
 void shuffleShoe(int shoe[], int shoeSize);
-int drawCard(int shoe[], int shoeSize, int* shoePos, int numDecks);
+int drawCard(int shoe[], int shoeSize, int &shoePos, int numDecks);
 
-//Functions for card management
+//Card management
 int getRank(int card);
 int getSuit(int card);
 int cardBaseValue(int card);
@@ -24,9 +25,9 @@ int cardBaseValue(int card);
 int handValue(const int hand[], int count);
 bool isBust(const int hand[], int count);
 bool isBlackjack(const int hand[], int count);
-void resetHand(int hand[], int* count, int maxSize);
 bool canSplitHand(const int hand[], int count);
 bool canDoubleDown(const int hand[], int count);
+void resetHand(int hand[], int &count, int maxSize);
 
 //Functions for displaying cards and hands to player
 void printCard(int card);
@@ -34,11 +35,8 @@ void printHand(const std::string &label, const int hand[], int count, bool hideF
 void printHands(const std::string &playerName, const int playerHand[], int playerCount, const int dealerHand[], int dealerCount, bool hideDealerFirst);
 
 //Functions to manage player and dealer turns
-void dealStartingHands(int shoe[], int shoeSize, int* shoePos, int numDecks, int playerHand[], int* playerCount, int dealerHand[], int* dealerCount);
-int getPlayerDecision(bool allowDoubleDown, bool allowSplit);//Returns 1=Hit, 2=Stand, 3=Double Down, 4=Split
-void playerTurn(int shoe[], int shoeSize, int* shoePos, int numDecks, int playerHand[], int* playerCount, const int dealerHand[], int dealerCount, const std::string &playerName);
-void dealerTurn(int shoe[], int shoeSize, int* shoePos, int numDecks, int dealerHand[], int* dealerCount);
-void playBlackjackRound(int shoe[], int shoeSize, int* shoePos, int numDecks, const std::string &playerName);
-
-//Function for processing user input
-int readInt();
+void dealStartingHands(int shoe[], int shoeSize, int &shoePos, int numDecks, int playerHand[], int &playerCount, int dealerHand[], int &dealerCount);
+int getPlayerDecision(bool allowDoubleDown, bool allowSplit);//Returns 1=Hit, 2=Stand, 3=Double Down, 4=Split, 5=Quit
+void playerTurn(int shoe[], int shoeSize, int &shoePos, int numDecks, int playerHand[], int &playerCount, const int dealerHand[], int dealerCount, const std::string &playerName, int &bankroll, int &bet, int fixedBet, bool allowSplitFirstDecision, bool &quitRound);
+void dealerTurn(int shoe[], int shoeSize, int &shoePos, int numDecks, int dealerHand[], int &dealerCount);
+bool playBlackjackRound(int shoe[], int shoeSize, int &shoePos, int numDecks, const std::string &playerName, int &bankroll);
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include "../include/questions.hpp"
@@ -5,7 +6,33 @@
 int main()
 {
     std::srand(std::time(0));//seed random number generator 
-    startBlackjack();//Start the blackjack game
+    
+    int bankroll = 1000;//Shared bankroll for player casino games
+    bool running = true;
+   
+    while (running == true)
+    {
+        showMainMenu();
+        int choice = getMenuChoice();
 
+        if (choice == 1)
+        {
+            startBlackjack(bankroll);
+        }
+        else if (choice == 2)
+        {
+            std::cout << "\nPoker (Coming Soon)\n";
+        }
+        else if (choice == 3)
+        {
+            std::cout << "\nRoulette (Coming Soon)\n";
+        }
+        else if (choice == 4)
+        {
+            running = false;
+        }
+    }
+
+    std::cout << "\nThank you for visiting the Casino! Your final bankroll is: $" << bankroll << "\n";
     return 0;
 }
